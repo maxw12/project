f31bfaa4bb9cf07d44862847cc8965fd9f206690
maxw12 <74052003+maxw12@users.noreply.github.com>
2022-02-27 09:38:25 +0000

    polls done

--- djangoProject2/polls/admin.py
+++ djangoProject2/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
 
 # Register your models here.
-admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub-date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
